e4d874a3ba62e1525a53f66893d944ba34979563
Martin Zhang <martinzhang@Martins-MacBook-Pro.local>
2017-03-14 16:09:17 -0700

    Decided colors for UI(programmatically since interface builder colors aren't matching with colors within Sketch 3)

--- Aim!/AimSessionSelectionMainViewController.swift
+++ Aim!/AimSessionSelectionMainViewController.swift
@@ -11,22 +11,41 @@ import Firebase
 
 @IBDesignable
 class AimSessionSelectionMainViewController: UIViewController {
-
+    
     let aimApplicationThemeFont24 = UIFont(name: "PhosphatePro-Inline", size: 24)
+    let aimApplicationThemeOrangeColor = hexStringToUIColor(hex: "FF4A1c")
+    let aimApplicationThemePurpleColor = hexStringToUIColor(hex: "1A1423")
+    let aimApplicationNavBarThemeColor = hexStringToUIColor(hex: "1A1421")
+    
+    @IBOutlet weak var aimSessionCollectionView: UICollectionView!
     @IBOutlet weak var userLoginStatusIndicatorLabel: UILabel!
     @IBOutlet weak var aimTokenSumLabel: UILabel!
+    @IBOutlet weak var aimTokenHourSeparaterImageView: UIImageView!
     @IBOutlet weak var aimHourSumLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Set background color: (For some reason I could not match the color I designed in Sketch 3 with the bg color I set in storyboard; therefore, I manually set hex color value onto each UIView element which needs a customized color)
+        self.view.backgroundColor = aimApplicationThemePurpleColor
+        
+        aimTokenSumLabel.textColor = aimApplicationThemeOrangeColor
+        aimTokenHourSeparaterImageView.backgroundColor = aimApplicationThemeOrangeColor
+        aimHourSumLabel.textColor = aimApplicationThemeOrangeColor
+        
+        self.navigationController?.navigationBar.barTintColor = aimApplicationNavBarThemeColor
         // Putting Aim! logo onto nav bar:
         let navBarAimLogo = UIImage(named: "aim!LogoForNavigationBar")
         self.navigationItem.titleView = UIImageView.init(image: navBarAimLogo)
+        // Setting nav bar item colors:
+        self.navigationItem.leftBarButtonItem?.tintColor = aimApplicationThemeOrangeColor
+        self.navigationItem.rightBarButtonItem?.tintColor = aimApplicationThemeOrangeColor
         
         // Use custom PhosphatePro-Inline font
         aimTokenSumLabel.font = aimApplicationThemeFont24
         aimHourSumLabel.font = aimApplicationThemeFont24
+        
+        aimSessionCollectionView.backgroundColor = aimApplicationThemePurpleColor
         
         // Check user login status
         if FIRAuth.auth()?.currentUser?.uid == nil {
