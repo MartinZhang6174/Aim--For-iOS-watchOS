6f45a5b288d3bfe0e6401350c4bc568e16048513
Martin Zhang <martinzhang@Martins-MacBook-Pro.local>
2017-05-30 18:51:49 -0700

    Have not finished code to establish watch connectivity but making solid progress.

--- Aim! For watchOS Extension/AimMainSessionSelectionInterfaceController.swift
+++ Aim! For watchOS Extension/AimMainSessionSelectionInterfaceController.swift
@@ -11,23 +11,43 @@ import Foundation
 
 
 class AimMainSessionSelectionInterfaceController: WKInterfaceController {
-    
-    @IBOutlet var aimSessionTable: WKInterfaceTable!
 
+    @IBOutlet var sessionTable: AimSessionSelectionTable!
+    
+    var s1 = AimSession(sessionTitle: "English HW", dateInitialized: nil, url: nil, priority: false)
+    var s2 = AimSession(sessionTitle: "Physics Proj", dateInitialized: nil, url: nil, priority: true)
+    var s3 = AimSession(sessionTitle: "Math Tutor", dateInitialized: nil, url: nil, priority: false)
+    var s4 = AimSession(sessionTitle: "iOS Program", dateInitialized: nil, url: nil, priority: true)
+    var sessionArray = [AimSession]()
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
+        sessionArray.append(s1)
+        sessionArray.append(s2)
+        sessionArray.append(s3)
+        sessionArray.append(s4)
+        
         // Configure interface objects here.
+        updateDisplay()
     }
-
-    override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
-        super.willActivate()
+    
+    private func updateDisplay() {
+        let numberOfSessions = sessionArray.count
+        sessionTable.setNumberOfRows(numberOfSessions, withRowType: "AimSessionRowType")
+        
+        for index in 0...(numberOfSessions-1) {
+            if let rowController = sessionTable.rowController(at: index) as? AimSessionRowController {
+                let session = sessionArray[index]
+                rowController.session = session
+            }
+        }
     }
-
-    override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
-        super.didDeactivate()
+    
+    override func contextForSegue(withIdentifier segueIdentifier: String, in table: WKInterfaceTable, rowIndex: Int) -> Any? {
+        if let rowController = sessionTable.rowController(at: rowIndex) as? AimSessionRowController {
+            return rowController.session
+        }
+        return nil
     }
-
 }
---
+++ Aim! For watchOS Extension/AimSessionInterfaceController.swift
@@ -0,0 +1,42 @@
+//
+//  AimSessionInterfaceController.swift
+//  Aim!
+//
+//  Created by Martin Zhang on 2017-05-27.
+//  Copyright © 2017 Martin Zhang. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class AimSessionInterfaceController: WKInterfaceController {
+    
+    @IBOutlet var sessionTitleLabel: WKInterfaceLabel!
+    @IBOutlet var sessionTimer: WKInterfaceTimer!
+    
+    var session: AimSession!
+    
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+        if let context = context as? AimSession {
+            session = context
+            
+            setTitle(session.title)
+            sessionTitleLabel.setText(session.title)
+        }
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
