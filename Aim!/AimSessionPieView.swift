9dcd940ef612b627a025a4495a10e1fc07c375fe
Martin Zhang <martinzhang@Martins-MacBook-Pro.local>
2017-07-08 18:24:09 -0700

    Great progress on UI and feature implementations.

---
+++ Aim!/AimSessionPieView.swift
@@ -0,0 +1,21 @@
+//
+//  AimSessionPieView.swift
+//  Aim!
+//
+//  Created by Martin Zhang on 2017-07-08.
+//  Copyright © 2017 Martin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class AimSessionPieView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
